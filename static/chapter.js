f5869807e09e9cbfcd9363ff9d3967c273dcb652
chiudaniels <daniel.sean.chiu@gmail.com>
2017-07-18 19:12:07 -0400

    Made basic mockup of invision website
    need to add functionality to the fanart icons

---
+++ static/chapter.js
@@ -0,0 +1,11 @@
+var fanart=document.getElementsByClassName("fanartIcon")
+var paragraph=document.getElementByClassName("paragraph")
+
+
+var iconClick = function (e){
+	
+}
+
+for (var i=0; i<paragraphs.length; i++){
+	paragraphs[i].addEventListener('click',iconClick(),false);
+}
