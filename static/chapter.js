05ac2d81396a64f18ae9d058ed7fa55c483f5930
chiudaniels <daniel.sean.chiu@gmail.com>
2017-07-24 15:27:22 -0400

    did some changes template
    new design. added basic file upload for testing

--- static/chapter.js
+++ static/chapter.js
@@ -6,27 +6,27 @@ var iconClick = function (pos){
 	console.log(pos);
 }
 
-var pixClixed = function (num){
-	if (picture[num].getAttribute("display") == "false"){
-		picture[num].style.display="inline";
-		picture[num].setAttribute("display","true")
-	}
-	else if (picture[num].getAttribute("display") == "true"){
-		picture[num].style.display="none";
-		picture[num].setAttribute("display","false");
-	}
-}
+// var pixClixed = function (num){
+// 	if (picture[num].getAttribute("display") == "false"){
+// 		picture[num].style.display="inline";
+// 		picture[num].setAttribute("display","true")
+// 	}
+// 	else if (picture[num].getAttribute("display") == "true"){
+// 		picture[num].style.display="none";
+// 		picture[num].setAttribute("display","false");
+// 	}
+// }
 
 
-for (var i=0; i<paragraph.length; i++){
-	fanart[i].setAttribute("num",i);
-	picture[i].setAttribute("display",false);
-	fanart[i].addEventListener('click',function(e){
-		console.log("clicked")
-		pixClixed(parseInt(e.target.getAttribute("num")));
-	}
-	,false);
-}
+// for (var i=0; i<paragraph.length; i++){
+// 	fanart[i].setAttribute("num",i);
+// 	picture[i].setAttribute("display",false);
+// 	fanart[i].addEventListener('click',function(e){
+// 		console.log("clicked")
+// 		pixClixed(parseInt(e.target.getAttribute("num")));
+// 	}
+// 	,false);
+// }
 
 
 $(document).ready(function() {
