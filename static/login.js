b8ec8920ee0e6c46b93e6b22b6149f621aab43ed
jeanne locker <altermuse3@gmail.com>
2017-07-25 13:54:36 -0400

    more weird ajax/html stuff'

--- static/login.js
+++ static/login.js
@@ -1,18 +1,29 @@
 /*$(document).ready(function() {
     $("#sign-in-button").click(function() {
-	$.ajax({
-	    type: "POST",
-	    url: '/login/',
-	    username: username;
-	    password: password;
-	    dataType: 'json',
-	})
-	    .done(function(resp){
-		if (resp == "fail"){
-		    document.getElementById("demo").innerHTML = "
-<div class='alert alert-danger'>
-  <strong>Whoops!</strong> Seems like your username or password is wrong.</div>"
-		}
-	    })
+	document.getElementById("errorMess").innerHTML = "HelloWorld";
     });
-});*/
+});
+  */  
+	/*
+
+	  $.ajax({
+	  type: "POST",
+	  url: '/login/',
+	  username: username;
+	  password: password;
+	  dataType: 'json',
+	  })
+	  .done(function(resp){
+	  if (resp == "fail"){
+	  document.getElementById("errorRegion").innerHTML = "
+	  <div class='alert alert-danger'>
+	  <strong>Whoops!</strong> Seems like your username or password is wrong.</div>"
+	  }
+	  })*/
+   /* });
+});
+ */
+
+function myFunction(){
+    console.log("HELP");
+}
