6aebf03cccde3a547cfcea97d84eb81b8d64a05a
chiudaniels <daniel.sean.chiu@gmail.com>
2017-08-14 10:42:38 -0400

    added potential fix

--- static/js/uploadChapter.js
+++ static/js/uploadChapter.js
@@ -48,13 +48,12 @@ $(document).ready(function() {
 
 var form = document.querySelector('form');
 form.onsubmit = function() {
-    // Populate hidden form on submit
-    var about = document.querySelector('input[name=storyText]');
-    about.value = JSON.stringify(quill.getContents());
-    t
-    console.log("Submitted", $(form).serialize(), $(form).serializeArray());
-
-    // No back end to actually submit to!
-    alert('Open the console to see the submit data!')
-    return false;
+  // Populate hidden form on submit
+  var about = document.querySelector('input[name=about]');
+  about.value = JSON.stringify(quill.getContents());
+  console.log("Submitted", $(form).serialize(), $(form).serializeArray());
+  
+  // No back end to actually submit to!
+  alert('Open the console to see the submit data!')
+  return false;
 };
