644ad17031ac6251a47b7d2f39bd2f6b999351a1
Zicheng Zhen <zichengzhen@gmail.com>
2017-08-30 10:47:48 -0400

    make book landing mobile friendly, resolves #34

--- static/js/bookLanding.js
+++ static/js/bookLanding.js
@@ -1,24 +1,4 @@
-var artArray = null;
-
-var loadBookLanding = function(){
-    $.ajax({
-	url: "/getBookLandingImages/",
-	type: "POST",
-	data: {
-	    "bookID": bookID
-	},
-	success: function(response){
-	    artArray = response;
-	    console.log(artArray);
-	},
-	error: function(data){
-	    console.log("book landing error");
-	}
-    });
-}
-
 $(function () {
-    loadBookLanding();    
     $('#art-slideshow').slick({
 	slidesToShow: 1,
 	slidesToScroll: 1,
